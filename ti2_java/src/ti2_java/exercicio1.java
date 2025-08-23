734c1335a0cb0064bb82621c1fbbcc533a8ee7ec
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-23 17:16:09 -0300

    teste

---
+++ ti2_java/src/ti2_java/exercicio1.java
@@ -0,0 +1,10 @@
+package ti2_java;
+import java.util.*;
+
+class exercicio1 {
+	public static Scanner sc = new Scanner (System.in);
+	public static void main (String[] args) {
+		int n1 = sc.nextInt(); int n2 = sc.nextInt();
+		System.out.println((n1 + n2));
+	}
+}

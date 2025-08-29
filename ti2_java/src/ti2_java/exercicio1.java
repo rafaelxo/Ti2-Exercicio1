c34e35788139f215e52032360fe4c25d0c13b954
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-29 09:39:41 -0300

    att

--- ti2_java/src/ti2_java/exercicio1.java
+++ ti2_java/src/ti2_java/exercicio1.java
@@ -4,7 +4,12 @@ import java.util.*;
 class exercicio1 {
 	public static Scanner sc = new Scanner (System.in);
 	public static void main (String[] args) {
-		int n1 = sc.nextInt(); int n2 = sc.nextInt(); // declaração e leitura dos dois inteiros
-		System.out.println((n1 + n2)); // saída na tela da soma dos dois inteiros
+		System.out.println("Soma de inteiros!\n");
+		System.out.print("Insira o primeiro valor: ");
+		int n1 = sc.nextInt(); // declaração e leitura do primeiro valor
+		System.out.print("Insira o segundo valor: ");
+		int n2 = sc.nextInt(); // declaração e leitura do segundo valor
+		System.out.println("O resultado da soma de " + n1 + " com " + n2 + " é: " + (n1 + n2)); // saída na tela do resultado da soma dos dois inteiros
+		System.out.println("\nFim do programa!");
 	}
 }
